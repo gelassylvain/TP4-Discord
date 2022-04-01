c9e6b0ce20deff55606171b46759511dde1f6c9d
Sylvain <gelas@et.esiea.fr>
2022-04-01 10:47:41 +0200

    3th commit

--- client.py
+++ client.py
@@ -5,7 +5,12 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print("Le bot est prêt !")
-client.run("OTU5MzUxNTI4MjQwNjA3Mjgz.YkanvA.dhp0cV7zxFSgkvUw8SkGdb2OsBs")
+
+@client.event
+async def on_message(message):
+    print(message.content)
+
+client.run("OTU5MzUxNTI4MjQwNjA3Mjgz.YkanvA.t_dvsKsybX8c8DVfIuRV8pAbKUw")
 
 
 '''class MyBot(Client):
