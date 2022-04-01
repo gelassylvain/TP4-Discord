ed75715995b12f9c8516ae5e4a1de0bdd7a998af
Sylvain <gelas@et.esiea.fr>
2022-04-01 10:39:46 +0200

    2thbis commit

--- client.py
+++ client.py
@@ -1,13 +1,18 @@
 import discord
 from discord import Client
 
-##ACTIVATION DU BOT
-##IL EST DESORMAIS EN LIGNE SUR DISCORD
 client = discord.Client()
-client.run("OTU5MzUxNTI4MjQwNjA3Mjgz.YkanvA.A2ISMx69gNUoHZWIm4NcshBnyu8")
-class MyBot(Client):
+@client.event
+async def on_ready():
+    print("Le bot est prêt !")
+client.run("OTU5MzUxNTI4MjQwNjA3Mjgz.YkanvA.dhp0cV7zxFSgkvUw8SkGdb2OsBs")
+
+
+'''class MyBot(Client):
     def init(self):
         super().init()
 
     async def on_ready(self):
-        self.log.infolog(f"{self.user} has connected to Discord!")
+        self.log.infolog(f"{self.user} has connected to Discord!") '''
+
+
